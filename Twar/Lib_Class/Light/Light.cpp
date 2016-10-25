658a3c384e6324372d03b28f1d4f3f7bac7e8d66
Sharoku-haga <beker221noa@yhaoo.co.jp>
2016-10-25 23:52:33 +0900

    LightクラスをUTF-8化

---
+++ Twar/Lib_Class/Light/Light.cpp
@@ -0,0 +1,105 @@
+﻿/**
+* @file  Light.h
+* @brief ライトに関するクラス
+* @author haga
+*/
+
+#include "Light.h"
+
+// コンストラクタ 環境光
+Light::Light(D3DXVECTOR3 direction) :
+m_pD3Device(GraphicsDevice::GetInstance().GetDevice()),
+m_direction(direction),
+m_ambientColor(0x00111111)
+{
+	ZeroMemory(&m_light, sizeof(D3DLIGHT9));
+	m_light.Type = D3DLIGHT_DIRECTIONAL;
+	m_light.Diffuse.r = 1.0f;
+	m_light.Diffuse.g = 1.0f;
+	m_light.Diffuse.b = 1.0f;
+	m_light.Specular.r = 1.0f;
+	m_light.Specular.g = 1.0f;
+	m_light.Specular.b = 1.0f;
+	D3DXVec3Normalize((D3DXVECTOR3*)&m_light.Direction, &m_direction);
+	m_light.Range = 500.0f;
+}
+
+// コンストラクタ 
+Light::Light(D3DXVECTOR3 lightPos,D3DXVECTOR3 direction):
+m_pD3Device(GraphicsDevice::GetInstance().GetDevice()),
+m_direction(direction),
+m_lightPos(lightPos),
+m_ambientColor(0x00111111)
+{
+	ZeroMemory(&m_light, sizeof(D3DLIGHT9));
+	m_light.Type = D3DLIGHT_SPOT;
+	m_light.Diffuse.r = 1.0f;
+	m_light.Diffuse.g = 1.0f;
+	m_light.Diffuse.b = 1.0f;
+	m_light.Specular.r = 1.0f;
+	m_light.Specular.g = 1.0f;
+	m_light.Specular.b = 1.0f;
+	m_light.Position = m_lightPos;
+	D3DXVec3Normalize((D3DXVECTOR3*)&m_light.Direction, &m_direction);
+	m_light.Range = 500.0f;
+}
+
+// デストラクタ
+Light::~Light()
+{
+}
+
+// ライト使用する関数
+void Light::LightUp()
+{
+
+
+	m_pD3Device->SetLight(0, &m_light);
+	m_pD3Device->LightEnable(0, TRUE);
+	m_pD3Device->SetRenderState(D3DRS_LIGHTING, TRUE);				     // ライトを有効にする
+	m_pD3Device->SetRenderState(D3DRS_AMBIENT, m_ambientColor);			 // アンビエントライト(環境光)を設定する
+	m_pD3Device->SetRenderState(D3DRS_SPECULARENABLE, TRUE);		     // スペキュラ(鏡面反射)を有効にする
+}
+ 
+// ライトの種類を設定する
+void Light::SetLightType(D3DLIGHTTYPE type)
+{
+	m_light.Type = type;
+}
+
+
+// ライトの座標と向きをセットする
+void Light::SetLightPos(D3DXVECTOR3 pos)
+{
+	m_light.Position = pos;
+
+}
+
+// ライトの向きをセットする
+void  Light::SetLightDirection(D3DXVECTOR3 direction)
+{
+	m_direction = direction;
+	D3DXVec3Normalize((D3DXVECTOR3*)&m_light.Direction, &direction);
+}
+
+// ライトの距離をセットする
+void Light::SetLightRange(float range)
+{
+	m_light.Range = range;
+}
+
+// ディヒューズをセットする
+void Light::SetDiffuse(float dR, float dG, float dB)
+{
+	m_light.Diffuse.r = dR;
+	m_light.Diffuse.g = dG;
+	m_light.Diffuse.b = dB;
+}
+
+// ライトのスペキュラをセットする
+void Light::SetSpecular(float dR, float dG, float dB)
+{
+	m_light.Specular.r = dR;
+	m_light.Specular.g = dG;
+	m_light.Specular.b = dB;
+}
---
+++ Twar/Lib_Class/Light/Light.h
@@ -0,0 +1,89 @@
+﻿/**
+* @file  Light.h
+* @brief ライトに関するクラス
+* @author haga
+*/
+#ifndef LIGHT_H
+#define LIGHT_H
+#include <d3dx9.h>
+#include "../GraphicsDevice.h"
+
+/**
+* ライトに関するクラス
+*/
+class Light
+{
+private:
+	LPDIRECT3DDEVICE9		m_pD3Device;		//!< Direct3Dのデバイス
+	D3DLIGHT9				m_light;			//!< ライト構造体
+	D3DXVECTOR3				m_lightPos;			//!< ライトの位置
+	D3DXVECTOR3             m_direction;		//!< ライトの向き
+	DWORD					m_ambientColor;		//!< アンビエント色
+	
+
+public:
+	/**
+	* コンストラクタ
+	* @param[in] direction ライトの向き
+	*/
+	Light(D3DXVECTOR3 direction);
+
+	/**
+	* コンストラクタ
+	* @param[in] lightPos  ライトの位置
+	* @param[in] direction ライトの向き
+	*/
+	Light(D3DXVECTOR3 lightPos,D3DXVECTOR3 direction);
+
+	/**デストラクタ*/
+	~Light();
+
+	/**
+	* ライトアップ関数
+	*/
+	void LightUp();
+
+	/**
+	* ライトの種類を設定する<br>
+	* D3DLIGHT_POINT,D3DLIGHT_SPOT,D3DLIGHT_DIRECTIONALの種類がある
+	* @param[in] type ライトの種類 
+	*/
+	void SetLightType(D3DLIGHTTYPE type);
+
+	/**
+	* ライトの座標と向きをセットする
+	* @param[in] pos ライトの座標
+	* @param[in] direction ライトの向き
+	*/
+	void SetLightPos(D3DXVECTOR3 pos);
+
+	/**
+	* ライトの向きをセットする
+	* @param[in] direction ライトの向き
+	*/
+	void SetLightDirection(D3DXVECTOR3 direction);
+
+	/**
+	* ライトの距離をセットする
+	* @param[in] range ライトの」届く距離
+	*/
+	void SetLightRange(float range);
+
+	/**
+	* ライトのディヒューズをセットする関数
+	* @param[in] dR R値 
+	* @param[in] dG G値
+	* @param[in] dB B値
+	*/
+	void SetDiffuse(float dR,float dG,float dB);
+
+	/**
+	* ライトのスペキュラをセットする関数
+	* @param[in] dR R値
+	* @param[in] dG G値
+	* @param[in] dB B値
+	*/
+	void SetSpecular(float dR, float dG, float dB);
+};
+
+#endif		// LIGHT_H
