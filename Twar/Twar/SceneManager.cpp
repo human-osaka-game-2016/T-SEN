3df3fcec6204484b67c6df8763deae1708b7a66f
Sharoku-haga <beker221noa@yhaoo.co.jp>
2016-10-30 04:07:23 +0900

    SceneManagerクラスのファイルを作成してコミット

---
+++ Twar/Twar/SceneManager.cpp
@@ -0,0 +1,16 @@
+/**
+* @file SceneManager.h
+* @brief シーンを管理するクラスSceneManagerのcpp
+* @author haga
+*/
+#include "SceneManager.h"
+
+
+SceneManager::SceneManager()
+{
+}
+
+
+SceneManager::~SceneManager()
+{
+}
