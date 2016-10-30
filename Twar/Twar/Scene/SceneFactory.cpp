9f2d101c3bf61ad5b0f1acf2ad348c3c0f2bd6bf
Sharoku-haga <beker221noa@yhaoo.co.jp>
2016-10-31 02:50:53 +0900

    SceneFactoryクラス追加

---
+++ Twar/Twar/Scene/SceneFactory.cpp
@@ -0,0 +1,40 @@
+﻿/**
+* @file SceneFactory.cpp
+* @brief  シーンを作成するクラスのcpp
+* @author haga
+*/
+#include "Scene.h"
+#include "../GameData/SaveDataManager.h"
+#include "SceneFactory.h"
+
+// 初期化関数
+void SceneFactory::Init(SaveDataManager* saveDataManager)
+{
+	m_pSaveDataManager = saveDataManager;
+}
+
+// シーンを作成する関数
+Scene* SceneFactory::CreateScene(SCENE_ID sceneID)
+{
+	Scene* pScene = nullptr;
+
+	switch (sceneID)
+	{
+	case LOGO_SCENE:
+		break;
+
+	case TITLE_SCENE:
+		break;
+
+	case GAME_SCENE:
+		break;
+
+	case ENDROLL_SCENE:
+		break;
+
+	default:
+		break;
+	}
+
+	return pScene;
+}
