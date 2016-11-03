d4371d454fa4f47445c80f9f0bc369d00c050149
Sharoku-haga <beker221noa@yhaoo.co.jp>
2016-11-03 16:43:42 +0900

    [ #37 ] GameDataManagerクラス作成
    -SubSceneクラスの作成に必要だったため、ファイルだけ作成

---
+++ Twar/Twar/GameData/GameDataManager.cpp
@@ -0,0 +1,17 @@
+﻿/**
+* @file GameDataManager.cpp
+* @brief ゲームデータを管理するクラスのcpp
+* @author haga
+* @note 2016/11/03ファイル作成したが、実装はまだ行っていない
+*/
+#include "GameDataManager.h"
+
+
+GameDataManager::GameDataManager()
+{
+}
+
+
+GameDataManager::~GameDataManager()
+{
+}
---
+++ Twar/Twar/GameData/GameDataManager.h
@@ -0,0 +1,18 @@
+﻿/**
+* @file GameDataManager.h
+* @brief ゲームデータを管理するクラスのh
+* @author haga
+* @note 2016/11/03ファイル作成したが、実装はまだ行っていない
+*/
+#ifndef GAMEDATA_MANAGER_H
+#define GAMEDATA_MANAGER_H
+
+
+class GameDataManager
+{
+public:
+	GameDataManager();
+	~GameDataManager();
+};
+
+#endif // !GAMEDATA_MANAGER_H
