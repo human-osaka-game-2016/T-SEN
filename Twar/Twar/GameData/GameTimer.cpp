e8bc8bbfdf9cf359976117560291d76cd75b2b21
Sharoku-haga <beker221noa@yhaoo.co.jp>
2016-11-03 17:43:04 +0900

    ファイル追加

---
+++ Twar/Twar/GameData/GameTimer.cpp
@@ -0,0 +1,17 @@
+﻿/**
+* @file GameTimer.cpp
+* @brief ゲーム内時間を管理するクラス
+* @author haga
+* @note 2016/11/03作成したが、実装はまだ行っていない
+*/
+#include "GameTimer.h"
+
+
+GameTimer::GameTimer()
+{
+}
+
+
+GameTimer::~GameTimer()
+{
+}
---
+++ Twar/Twar/GameData/GameTimer.h
@@ -0,0 +1,16 @@
+﻿/**
+* @file GameTimer.h
+* @brief ゲーム内時間を管理するクラス
+* @author haga
+* @note 2016/11/03作成したが、実装はまだ行っていない
+*/
+#ifndef GAME_TIMER_H
+#define GAME_TIMER_H
+class GameTimer
+{
+public:
+	GameTimer();
+	~GameTimer();
+};
+
+#endif // GAME_TIMER_H
