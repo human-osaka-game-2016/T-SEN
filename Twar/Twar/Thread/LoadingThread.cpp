fb35633bda9e3c849d1f34153038b53c575ba3b8
Sharoku-haga <beker221noa@yhaoo.co.jp>
2016-11-21 09:56:19 +0900

    [enh #61] LoadingThreadクラス作成
    -LoadingThreadクラスの追加
    -モノステイトパターンを使用
    -レビューによる修正

--- Twar/Twar/Thread/LoadingThread.cpp
+++ Twar/Twar/Thread/LoadingThread.cpp
@@ -1,11 +1,111 @@
-﻿#include "LoadingThread.h"
+﻿/**
+* @file		LoadingThread.cpp
+* @brief	LoadingThreadクラス実装
+* @author	haga
+*/
 
+//-------------------------------------------------------------------------------------------------//
+//Includes
+//-------------------------------------------------------------------------------------------------//
+#include "GameLib/GameLib.h"
+#include "LoadingThread.h"
+
+//-------------------------------------------------------------------------------------------------//
+//Static private variables
+//-------------------------------------------------------------------------------------------------//
+std::thread*	LoadingThread::m_pThread = nullptr;
+bool			LoadingThread::m_IsLoading = false;
+int				LoadingThread::m_TexID = INT_MAX;			// 登録されないだろう数値を代入している
+
+//-----------------------------------------------------------------------------------------------------//
+//Static public functions
+//-----------------------------------------------------------------------------------------------------//
+
+//	スレッドを作成する関数
+void LoadingThread::CreateThread(int texID)
+{
+	if(m_pThread != nullptr)			// スレッドがあるかどうかチェック
+	{
+		MessageBox(0, "スレッドがすでに作成されています。", NULL, MB_OK);
+		return;
+	}
+
+	m_TexID = texID;
+	m_IsLoading = true;
+	m_pThread = new std::thread(LoopLoadingScreen);
+}
+
+// スレッドを破棄する関数
+void LoadingThread::DiscardThread()
+{
+	if(m_pThread == nullptr)		// スレッドがあるかどうかチェック
+	{
+		MessageBox(0, "このスレッドはありません。", NULL, MB_OK);
+		return;
+	}
+
+	m_IsLoading = false;
+
+	m_pThread->join();				// スレッドが終了するまで待機する
+
+	delete m_pThread;
+	m_pThread = nullptr;
+	m_TexID = INT_MAX;				// 登録されないだろう数値を代入している
+}
+
+//-----------------------------------------------------------------------------------------------------//
+//Static private functions
+//-----------------------------------------------------------------------------------------------------//
+
+// ループ関数(スレッドの実体)
+void LoadingThread::LoopLoadingScreen()
+{
+	CUSTOMVERTEX loadingScreen[] =
+	{
+		{	 0,	  0, 0.5f, 1.0f, 0xFFFFFFFF, 0.0f, 0.0f },
+		{ 1600,   0, 0.5f, 1.0f, 0xFFFFFFFF, 1.0f, 0.0f },
+		{ 1600, 900, 0.5f, 1.0f, 0xFFFFFFFF, 1.0f, 1.0f },
+		{	 0, 900, 0.5f, 1.0f, 0xFFFFFFFF, 0.0f, 1.0f },
+	};
+
+	DWORD currentTime;					// 現在の時間
+	DWORD oldTime = timeGetTime();		// 前の時間
+
+	while (m_IsLoading)
+	{
+		currentTime = timeGetTime();				
+		if(currentTime - oldTime >= ( 1000 / 60 ))
+		{
+			Control(loadingScreen, sizeof(loadingScreen) / sizeof(CUSTOMVERTEX));
+			Draw(loadingScreen);
+			oldTime = currentTime;
+		}
+	}
+}
+
+// コントロール関数
+void LoadingThread::Control(CUSTOMVERTEX vtx[], int vtxNum)
+{
+	for(int i = 0; i < vtxNum; i++)
+	{
+		vtx[i].tu += 0.005f;
+	}
+}
+
+// 描画関数
+void LoadingThread::Draw(CUSTOMVERTEX vtx[])
+{
+	GameLib::Instance().StartRender();
+	GameLib::Instance().GetDevice()->SetTexture(0, GameLib::Instance().GetTexture(m_TexID));
+	GameLib::Instance().GetDevice()->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 2, vtx, sizeof(CUSTOMVERTEX));
+	GameLib::Instance().EndRender();
+}
+
+//-------------------------------------------------------------------------------------------------//
+//Private functions
+//-------------------------------------------------------------------------------------------------//
 
 LoadingThread::LoadingThread()
 {
 }
 
-
-LoadingThread::~LoadingThread()
-{
-}
--- Twar/Twar/Thread/LoadingThread.h
+++ Twar/Twar/Thread/LoadingThread.h
@@ -1,8 +1,59 @@
-﻿#pragma once
+﻿/**
+* @file		LoadingThread.h
+* @brief	LoadingThreadクラスヘッダ
+* @author	haga
+* @todo		2016/11/21 作成したが、ライブラリの機能不足のため頂点情報がむき出しになっているので後で修正します。
+* 
+*/
+#ifndef LOADING_THREAD_H
+#define LOADING_THREAD_H
+
+#include <d3dx9.h>
+#include <thread>
+#include "GameLib/Lib/Vertex.h"
+
+/**
+* ロード画面を管理するクラス.
+*/
 class LoadingThread
 {
 public:
+	/**
+	* スレッドを作成する関数.
+	* @param texID	ロード画面で使用するテクスチャーのID
+	*/
+	static void CreateThread(int texID);
+
+	/**スレッドを破棄する関数*/
+	static void DiscardThread();
+
+private:
+	static std::thread*			m_pThread;					//!< threadクラスのインスタンスへのポインタ
+	static int					m_TexID;					//!< ロード画面に使用するテクスチャーのID
+	static bool					m_IsLoading;				//!< ロード中かどうかのフラグ
+
+	/**ループ関数(スレッドの実体)*/
+	static void LoopLoadingScreen();
+
+	/**
+	* コントロール関数.
+	* @param[in] vtx	ロード画面の頂点情報配列の先頭アドレス
+	* @param[in] vtxNum ロード画面の頂点の数
+	*/
+	static void Control(CUSTOMVERTEX vtx[], int vtxNum);
+
+	/**
+	* 描画関数
+	* @param[in] vtx ロード画面の頂点情報配列の先頭アドレス
+	*/
+	static void Draw(CUSTOMVERTEX vtx[]);
+
+	/**
+	* コンストラクタ<br>
+	* モノステイトパターンの為private
+	*/
 	LoadingThread();
-	~LoadingThread();
+
 };
 
+#endif	// LOADING_THREAD_H
