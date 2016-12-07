cb0496cca5b1a69aea70cbdf9176361b493e96e9
kakeru4 <kakeru.0413-sao@i.softbank.jp>
2016-12-07 15:35:03 +0900

    [enh#106]Bulletクラス
    - 一旦上げ
    
    - 一旦上げ
    
    - 今日の作業
    
    - いらないから削除
    
    - 入れ忘れ
    
    - コメント追加
    
    - 修正しわすれ

--- Twar/Twar/Bullet/Bullet.cpp
+++ Twar/Twar/Bullet/Bullet.cpp
@@ -1,1 +1,36 @@
-﻿
+﻿/**
+* @file  Bullet.cpp
+* @brief Bulletクラス実装
+* @author Matsuda
+*/
+#include"Bullet.h"
+#include"GameLib/GameLib.h"
+
+Bullet::Bullet()
+{
+}
+
+Bullet::~Bullet()
+{
+}
+
+void Bullet::Control(D3DXVECTOR3 Pos, float Rotate)
+{
+}
+
+void Bullet::Draw()
+{
+}
+
+void Bullet::PushBack(Data* pData)
+{
+}
+
+Bullet::Data * Bullet::EraseNext(Data* pData)
+{
+	return pData;
+}
+
+void Bullet::Clear()
+{
+}
--- Twar/Twar/Bullet/Bullet.h
+++ Twar/Twar/Bullet/Bullet.h
@@ -1,1 +1,62 @@
-﻿
+﻿/**
+* @file Bullet.h
+* @brief Bulletクラスヘッダ
+* @author Matsuda
+*/
+#ifndef BULLET_H
+#define BULLET_H
+
+#include <d3dx9.h>
+
+class FbxModel;
+
+/**
+*Bulletクラス
+*/
+class Bullet
+{
+	friend class BulletManager;
+
+protected:
+	struct Data
+	{
+		D3DXVECTOR3 BulletPos;
+		float Rotate;
+		bool hasDrawn;
+		bool hasInited;
+
+		Data* pBefore;
+		Data* pNext;
+		D3DXMATRIX Matrix;
+	};
+	Data* m_pFirst;
+	Data* m_pLast;
+	Data m_Data;
+
+public:
+	/**コンストラクタ*/
+	Bullet();
+
+	/**デストラクタ*/
+	virtual ~Bullet();
+
+	/**コントロール関数*/
+	virtual void Control(D3DXVECTOR3 Pos, float Rotate);
+	
+	/**描画関数*/
+	virtual void Draw();
+	
+	/**最後尾につける関数*/
+	virtual void PushBack(Data* pData);
+	
+	/**今あるポインターを削除して次のポインターを返す*/
+	virtual Data* EraseNext(Data* pData);
+	
+	/**リストを空にする関数*/
+	virtual void Clear();
+
+protected:
+	FbxModel* m_pFbx; //!<  モデルデータ
+
+};
+#endif  //BULLET_H
