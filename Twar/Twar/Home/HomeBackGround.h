e8e8419b58fbbdc342f2e9d3aea5348b2b144e68
Sharoku-haga <beker221noa@yahoo.co.jp>
2016-12-15 15:16:50 +0900

    [enh #119]  Homeシーンの実装(描画)
    -HomeBackgroundクラス実装
    -HomeMenuクラス実装
    -HomeSceneの実装
    -タイトルシーン後 Homeシーンに移行する形に変更

--- Twar/Twar/Home/HomeBackGround.h
+++ Twar/Twar/Home/HomeBackGround.h
@@ -1,8 +1,32 @@
-﻿#pragma once
+﻿/**
+* @file		HomeBackground.h
+* @brief	HomeBackgroundクラスヘッダ
+* @author	haga
+*/
+
+#ifndef HOME_BACKGROUND_H
+#define HOME_BACKGROUND_H
+
+#include <d3dx9.h>
+
+/**
+* Homeシーン(鎮守府)の背景を描画するクラス
+*/
 class HomeBackground
 {
 public:
+	/**コンストラクタ*/
 	HomeBackground();
+
+	/**デストラクタ*/
 	~HomeBackground();
+
+	/**描画関数*/
+	void Draw();
+
+private:
+	D3DXVECTOR2			m_Pos;		//!< 位置座標
+
 };
 
+#endif	// HOME_BACKGROUND_H
--- Twar/Twar/SubScene/Home.cpp
+++ Twar/Twar/SubScene/Home.cpp
@@ -1,33 +1,87 @@
 ﻿/**
-* @file Home.cpp
-* @brief Homeクラス実装
-* @author haga
+* @file		Home.cpp
+* @brief	Homeクラス実装
+* @author	haga
 */
+
+//------------------------------------------------------------------------------------------------------------//
+//Includes
+//------------------------------------------------------------------------------------------------------------//
+
 #include "GameLib/GameLib.h"
 #include "../GameData/GameDataManager.h"
 #include "../GameData/GameTimer.h"
 #include "../GameData/SaveDataManager.h"
 #include "Home.h"
+#include "../Home/HomeBackGround.h"
+#include "../Home/HomeMenu.h"
+
+//-----------------------------------------------------------------------------------------------------------//
+//Namespace
+//-----------------------------------------------------------------------------------------------------------//
 
 namespace sub_scene
 {
 
+//----------------------------------------------------------------------------------------------------------//
+//Public functions
+//----------------------------------------------------------------------------------------------------------//
+
 Home::Home(GameDataManager* pGameDataManager, GameTimer* pGameTimer, SaveDataManager* pSaveDataManager)
 	: SubScene(pGameDataManager, pGameTimer, pSaveDataManager)
+	, m_pBackground(new HomeBackground())
+	, m_pHomeMenu(new HomeMenu())
+	, m_CurrentState(Home::NOMAL)
 {
+	InitHomeScene();
 }
 
 Home::~Home()
 {
+	m_rGameLib.ReleaseAllVertex();
+	m_rGameLib.ReleaseAllTex();
+	delete m_pHomeMenu;
+	delete m_pBackground;
 }
 
 SUBSCENE_ID Home::Control()
 {
+	switch(m_CurrentState)
+	{
+	case Home::BATTLE_START:
+		return SUBSCENE_ID::BATTLE;
+
+		break;
+
+	case Home::WORLD_MAP_START:
+		break;
+
+	case Home::SHIPYARD_START:
+		break;
+
+	case Home::NOMAL:
+		m_CurrentState = m_pHomeMenu->Control();
+		break;
+	}
+
 	return SUBSCENE_ID::HOME;
 }
 
 void Home::Draw()
 {
+	m_pBackground->Draw();
+	m_pHomeMenu->Draw();
+}
+
+//-----------------------------------------------------------------------------------------------------------//
+//Private functions
+//-----------------------------------------------------------------------------------------------------------//
+
+void Home::InitHomeScene()
+{
+	/**@todo 仮画像のﾊﾟｽにしておく*/
+	m_rGameLib.LoadTexEx(Home::BACKGROUND_TEX, "../Resouce/tsentitle.png", 255, 0, 0, 0, false);
+	m_rGameLib.LoadTexEx(Home::MENU_BTN_TEX, "../Resouce/title_button.png", 255, 0, 0, 0, false);
 }
 
 }
