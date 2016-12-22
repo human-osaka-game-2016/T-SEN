b3f0f1b6f57c8e7a8b09a1d03dd1ce51346c72de
Sharoku-haga <beker221noa@yahoo.co.jp>
2016-12-23 01:52:03 +0900

    [enh #134]MonsterBulletクラス関連の実装
    -Monsterbulletクラス実装
    -LongRangeBulletクラスの実装
    -ShortRangeBulletクラスの実装
    -MonsterBulletManagerクラスの実装

--- Twar/Twar/Monster/MonsterBullet/LongRangeBullet.cpp
+++ Twar/Twar/Monster/MonsterBullet/LongRangeBullet.cpp
@@ -4,14 +4,116 @@
 * @author	haga
 */
 
+//-------------------------------------------------------------------------------------------------------------------------------------//
+//Includes
+//-------------------------------------------------------------------------------------------------------------------------------------//
+
+#include "GameLib/GameLib.h"
+#include "Fbx/FbxRelated.h"
 #include "LongRangeBullet.h"
 
+//-------------------------------------------------------------------------------------------------------------------------------------//
+//Namespace
+//-------------------------------------------------------------------------------------------------------------------------------------//
 
-LongRangeBullet::LongRangeBullet()
+namespace
 {
+
+const float		FiringHeight		= 50.f;				// 弾の発射する高さ
+const float		BulletSpeed			= 3.0f;				// 弾の速度
+const float		BulletRange			= 800.f;			// 弾の高さを保ってられる範囲.この範囲を過ぎると徐々に降下していく
+const float		DescendingSpeed		= 1.0f;				// 弾の降下速度(弾の射程範囲を超える場合び落下するときの速度)
+const float		PosYMinLimit		= -50.f;			// Y軸における位置座標の最小の限界値
+const float		RollingSpeed		= 0.5f;				// 弾の回転速度(回転角度)
+const float		ScaleVal			= 5.0f;				// 弾の大きさの倍率
+
 }
 
+//-------------------------------------------------------------------------------------------------------------------------------------//
+//Public functions
+//-------------------------------------------------------------------------------------------------------------------------------------//
+
+LongRangeBullet::LongRangeBullet(FbxModel* pModel, const D3DXVECTOR3& rPos, float radian)
+	: MonsterBullet(pModel, rPos, radian)
+	, m_FlyingDistanceCount({0.0f, 0.0f, 0.0f})
+	, m_RollingAngle(0.0f)
+	, m_IsWithinRange(true)
+{
+	m_Pos.y += FiringHeight;
+	m_BulletSpeedX = static_cast<float>(cos(m_Radian) * BulletSpeed);
+	m_BulletSpeedZ = static_cast<float>(sin(m_Radian) * BulletSpeed);
+}
 
 LongRangeBullet::~LongRangeBullet()
 {
+	// モデルのリソース解放はMonsterBulletManagerにて行う
+}
+
+// コントロール関数
+bool LongRangeBullet::Control()
+{
+	if(m_IsWithinRange)
+	{
+		m_Pos.x += m_BulletSpeedX;
+		m_Pos.z += m_BulletSpeedZ;
+		m_FlyingDistanceCount.x += m_BulletSpeedX;
+		m_FlyingDistanceCount.z += m_BulletSpeedZ;
+
+		// 弾が範囲内にあるかチェック
+		if(m_FlyingDistanceCount.x >  BulletRange || m_FlyingDistanceCount.x < -BulletRange ||
+			m_FlyingDistanceCount.z > BulletRange || m_FlyingDistanceCount.z < -BulletRange)
+		{
+			m_IsWithinRange = false;
+		}
+	}
+	else
+	{
+		m_Pos.x += m_BulletSpeedX;
+		m_Pos.z += m_BulletSpeedZ;
+		m_Pos.y -= DescendingSpeed;
+
+		if(m_Pos.y < PosYMinLimit)
+		{
+			m_HasVanished = true;
+		}
+	}
+
+	// 弾の回転処理
+	m_RollingAngle += RollingSpeed;
+	if(m_RollingAngle > 360.f)
+	{ 
+		m_RollingAngle -= 360.f;		// 正規化
+	}
+
+	return m_HasVanished;
+}
+
+// 描画関数
+void LongRangeBullet::Draw()
+{
+	D3DXMatrixIdentity(&m_MatWorld);	// 単位行列
+
+	// 拡大
+	D3DXMATRIX matScale;
+	D3DXMatrixScaling(&matScale, ScaleVal, ScaleVal, ScaleVal);
+	matScale._41 = 0.0f;
+	matScale._42 = 0.0f;
+	matScale._43 = 0.0f;
+	m_MatWorld *= matScale;
+
+	// 回転
+	D3DXMATRIX	matBank;
+	D3DXMatrixRotationZ(&matBank, D3DXToRadian(m_RollingAngle));
+	D3DXMatrixMultiply(&m_MatWorld, &m_MatWorld, &matBank);
+
+	D3DXMATRIX	matHeading;												
+	D3DXMatrixRotationY(&matHeading, m_Radian);
+	D3DXMatrixMultiply(&m_MatWorld, &m_MatWorld, &matHeading);
+
+	// 移動
+	D3DXMATRIX	matPosition;	// 位置座標行列
+	D3DXMatrixTranslation(&matPosition, m_Pos.x, m_Pos.y, m_Pos.z);
+	D3DXMatrixMultiply(&m_MatWorld, &m_MatWorld, &matPosition);
+	GameLib::Instance().GetDevice()->SetTransform(D3DTS_WORLD, &m_MatWorld);
+	m_pModel->DrawFbx();
 }
--- Twar/Twar/Monster/MonsterBullet/MonsterBulletManager.cpp
+++ Twar/Twar/Monster/MonsterBullet/MonsterBulletManager.cpp
@@ -4,14 +4,106 @@
 * @author	haga
 */
 
+//--------------------------------------------------------------------------------------------------------------------------------------//
+//Includes
+//--------------------------------------------------------------------------------------------------------------------------------------//
+
+#include "Fbx/FbxRelated.h"
 #include "MonsterBulletManager.h"
+#include "MonsterBullet.h"
+#include "LongRangeBullet.h"
+#include "ShortRangeBullet.h"
 
+//--------------------------------------------------------------------------------------------------------------------------------------//
+//Namespace
+//--------------------------------------------------------------------------------------------------------------------------------------//
 
-MonsterBulletManager::MonsterBulletManager()
+// 値は仮置き
+namespace
 {
+
+const int		FiredBulletNum = 50;				// 近距離弾の発射回数
+const float		FiringRadian = 0.1256f;				// 近距離弾の発射方向(ラジアンを格納するため、変数名はradian)
+
 }
 
+//--------------------------------------------------------------------------------------------------------------------------------------//
+//Public functions
+//--------------------------------------------------------------------------------------------------------------------------------------//
+
+MonsterBulletManager::MonsterBulletManager()
+	: m_pLongRangeBulletModel(new FbxRelated())
+	, m_pShortRangeBulletModel(new FbxRelated())
+{
+	// モデルデータ読み込み
+	/**@todo 2016/12/23現在モデルデータは仮置き*/
+	if(!m_pLongRangeBulletModel->LoadFbx("fbx/tenryu.fbx"))
+	{
+		// 読み込み失敗したらエラー
+		MessageBox(0, "遠距離弾のFBXファイルの読み込みに失敗しました。", NULL, MB_OK);
+	}
+
+	if(!m_pShortRangeBulletModel->LoadFbx("fbx/tenryu.fbx"))
+	{
+		// 読み込み失敗したらエラー
+		MessageBox(0, "近距離弾のFBXファイルの読み込みに失敗しました。", NULL, MB_OK);
+	}
+}
 
 MonsterBulletManager::~MonsterBulletManager()
 {
+	for(auto pBullet : m_pBullets)
+	{
+		delete pBullet;
+	}
+
+	delete m_pShortRangeBulletModel;
+	delete m_pLongRangeBulletModel;
+}
+
+// コントロール関数
+void MonsterBulletManager::Control()
+{
+	for(auto itr = m_pBullets.begin(); itr != m_pBullets.end();)
+	{
+		if((*itr)->Control())
+		{
+			delete (*itr);
+			itr = m_pBullets.erase(itr);
+			continue;
+		}
+		++itr;
+	}
+}
+
+// 描画関数
+void MonsterBulletManager::Draw()
+{
+	for(auto itr = m_pBullets.begin(); itr != m_pBullets.end();)
+	{
+		(*itr)->Draw();
+		++itr;
+	}
+}
+
+// 遠距離弾を生成する関数
+void MonsterBulletManager::CreateLongeRangeBullet(const D3DXVECTOR3& rPos, float radian)
+{
+	m_pBullets.push_back(new LongRangeBullet(m_pLongRangeBulletModel->m_pModel, rPos, radian));
+}
+
+// 近距離弾を生成する関数
+void MonsterBulletManager::CreateShortRangeBullet(const D3DXVECTOR3& rPos)
+{
+	float firingRadian = 0.0f;
+
+	for(int i = 0; i < FiredBulletNum; ++i)
+	{
+		firingRadian += (FiringRadian * i);
+
+		// 一方向に3発発射する(処理が重くなったら減らす)
+		m_pBullets.push_back(new ShortRangeBullet(m_pLongRangeBulletModel->m_pModel, rPos, firingRadian));
+		m_pBullets.push_back(new ShortRangeBullet(m_pLongRangeBulletModel->m_pModel, rPos, firingRadian));
+		m_pBullets.push_back(new ShortRangeBullet(m_pLongRangeBulletModel->m_pModel, rPos, firingRadian));
+	}
 }
