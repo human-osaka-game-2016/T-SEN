75eaffe87a010a0ae88485e8fddee6510f516761
Sharoku-haga <beker221noa@yahoo.co.jp>
2016-12-29 03:28:55 +0900

    [enh #134] Monsterクラス関連の再実装
    -Monsterクラス再実装
    -上記に伴うMonsterManagerクラスの再実装

--- Twar/Twar/Monster/MonsterManager.h
+++ Twar/Twar/Monster/MonsterManager.h
@@ -12,22 +12,31 @@
 class GameDataManager;
 class FbxRelated;
 class Monster;
+class MonsterBulletManager;
 
 class MonsterManager
 {
 public:
-	MonsterManager(GameDataManager* pGameDataManager);
+	/**
+	* コンストラクタ.
+	* @param pGameDataManager	GameDataManagerクラスのインスタンスへのポインタ	
+	*/
+	explicit MonsterManager(GameDataManager* pGameDataManager);
 
+	/**デストラクタ*/
 	~MonsterManager();
 
+	/**コントロール関数*/
 	void Control();
 
+	/**描画関数*/
 	void Draw();
 
 private:
 	GameDataManager*		m_pGameDataManager;		//!< GameDatamanagerクラスのインスタンスへのポインタ
 	FbxRelated*				m_pMonsterFbx;			//!< FbxRelatedクラスのインスタンスへのポインタ(モンスターのモデル)
-	std::vector<Monster*>	m_pMonster;				//!< Monsterクラスへのインスタンスへのポインタ
+	std::vector<Monster*>	m_pMonsters;			//!< Monsterクラスへのインスタンスへのポインタ
+	MonsterBulletManager*	m_pBulletManager;		//!< MonsterBulletManagerクラスへのインスタンスへのポインタ
 
 };
 
