57d71ac0f2925c592dded187504fa7a6b9e2dfee
Sharoku-haga <beker221noa@yahoo.co.jp>
2017-01-25 02:39:46 +0900

    [enh #146]  作業に必要なファイル追加

---
+++ Twar/Twar/SubScene/BattleResult/ResultBackground.h
@@ -0,0 +1,18 @@
+﻿/**
+* @file		ResultBackground.h		
+* @brief	ResultBackgroundクラスヘッダ
+* @auhtor	haga
+*/
+
+#ifndef RESULT_BACKGROUND_H
+#define RESULT_BACKGROUND_H
+
+class ResultBackground
+{
+
+public:
+	ResultBackground();
+	~ResultBackground();
+};
+
+#endif	// RESULT_BACKGROUND_H
---
+++ Twar/Twar/SubScene/BattleResult/ScoreResult.h
@@ -0,0 +1,18 @@
+﻿/**
+* @file		ScoreResult.h
+* @brief	ScoreResultクラスヘッダ
+* @auhtor	haga
+*/
+
+#ifndef SCORE_RESULT_H
+#define SCORE_RESULT_H
+
+class ScoreResult
+{
+
+public:
+	ScoreResult();
+	~ScoreResult();
+};
+
+#endif // SCORE_RESULT_H
